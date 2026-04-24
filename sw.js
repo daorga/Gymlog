96cb5a2033c2e8908841b6630e7893f932ea584d
daorga <daorga@gmail.com>
2026-04-24 16:50:17 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const VERSION = 'gymlog-v' + Date.now();
+const CACHE = 'gymlog-' + Date.now();
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(['./', './index.html']))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('message', e => {
+  if(e.data && e.data.action === 'skipWaiting') self.skipWaiting();
+});
