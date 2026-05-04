7925ca0d89d3fb27c8f5664b640a21a488726833
daorga <daorga@gmail.com>
2026-05-04 15:10:43 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,38 @@
-const VERSION = 'gymlog-v' + Date.now();
-const CACHE = 'gymlog-' + Date.now();
+const CACHE = 'gymlog-v1';
+const ASSETS = ['./', './index.html'];
 
+// Install: cache everything immediately
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE)
-      .then(c => c.addAll(['./', './index.html']))
+      .then(c => c.addAll(ASSETS))
       .then(() => self.skipWaiting())
   );
 });
 
+// Activate: delete old caches
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
 
+// Fetch: cache first, then network
 self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match(e.request))
+    caches.match(e.request).then(cached => {
+      if(cached) return cached;
+      return fetch(e.request).then(res => {
+        if(res && res.status === 200){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
   );
 });
 
